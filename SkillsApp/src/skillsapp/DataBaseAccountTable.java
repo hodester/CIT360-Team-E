e27d8609c141e8b333533a6e524c61a6307793e4
mhodes <mhodes@A121768.WASHOE.WCSD>
2016-12-13 19:35:25 -0800

    Updated the imports for DataBaseTable.java

--- SkillsApp/src/skillsapp/DataBaseAccountTable.java
+++ SkillsApp/src/skillsapp/DataBaseAccountTable.java
@@ -13,7 +13,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import org.hibernate.annotations.Table;
+import javax.persistence.Table;
+
+
 
 
 
